c67e3620e54735a6c0ef6bade72655787824378d
Matthew Pringle <matt@mattpringle.co.uk>
2020-02-12 20:59:56 +0000

    Rework

--- bin/index-p.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-'use strict';
-
-/* Ember Capacitor */
-/* ---------------------------------------------------------------------------------------------------- */
-const emberCapacitor = require( '../libs/ember-capacitor' );
-//emberCapacitor.run( process.argv );
