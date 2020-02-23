7a5af57f10c68b13cec5fcc449bd2d06f5423323
Matthew Pringle <matt@mattpringle.co.uk>
2020-02-23 18:21:59 +0000

    Commands

--- lib-old/commands/init.js
+++ lib-old/commands/init.js
@@ -16,47 +16,47 @@ module.exports = {
         try {
             
             /* Message */
-            process.stdout.write( ' - Init\r\n' );
+            //process.stdout.write( ' - Init\r\n' );
             
             /* Check App Name */
-            if ( args[ 3 ] === undefined || !utils.appName( args[ 3 ] ) ) {
-                throw 'Please enter a valid App Name (example-app)';
-            }
+            //if ( args[ 3 ] === undefined || !utils.appName( args[ 3 ] ) ) {
+            //    throw 'Please enter a valid App Name (example-app)';
+            //}
             
             /* Check App ID */
-            if ( args[ 4 ] === undefined || !utils.appID( args[ 4 ] ) ) {
-                throw 'Please enter an App Package ID (in Java package format, no dashes) (com.example.app)';
-            }
+            //if ( args[ 4 ] === undefined || !utils.appID( args[ 4 ] ) ) {
+            //    throw 'Please enter an App Package ID (in Java package format, no dashes) (com.example.app)';
+            //}
             
             /* Check File Exists */
-            if ( !utils.fileExists( './package.json' ) ) {
-                throw 'Unable to find package.json.';
-            }
+            //if ( !utils.fileExists( './package.json' ) ) {
+            //    throw 'Unable to find package.json.';
+            //}
             
             /* Check File Valid */
-            if ( !utils.valid() ) {
-                throw 'Invalid package.json';
-            }
+            //if ( !utils.valid() ) {
+            //    throw 'Invalid package.json';
+            //}
             
             /* Check Ember Added */
-            if ( !ember.added() ) {
-                throw 'An Ember app must be created before running ember-cap init';
-            }
+            //if ( !ember.added() ) {
+            //    throw 'An Ember app must be created before running ember-cap init';
+            //}
             
             /* Check Capacitor Added */
-            if ( capacitor.added() ) {
-                throw 'Capacitor already installed';
-            }
+            //if ( capacitor.added() ) {
+            //    throw 'Capacitor already installed';
+            //}
             
             /* Install Capacitor */
-            if ( !capacitor.install() ) {
-                throw 'Unable to install Capacitor';
-            }
+            //if ( !capacitor.install() ) {
+            //    throw 'Unable to install Capacitor';
+            //}
             
             /* Init Capacitor */
-            if ( !capacitor.init( args[ 3 ] , args[ 4 ] ) ) {
-                throw 'Unable to initiate Capacitor';
-            }
+            //if ( !capacitor.init( args[ 3 ] , args[ 4 ] ) ) {
+            //    throw 'Unable to initiate Capacitor';
+            //}
             
         /* Error */
         /* -------------------------------------------------------------------------------------------- */
